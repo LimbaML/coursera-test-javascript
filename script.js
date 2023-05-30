02ee38c2da4c3c65a905926ea5d5baaaf5d1fc29
LIMBA ML <127320850+LimbaML@users.noreply.github.com>
2023-05-30 20:58:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function(){
-  var names = ["yoo", "John", "Jack", "Jin", "Paul", "Rose", "Lassie", "Romeo", "Juliet", "Jacob"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for ( var i = 0; i < names.length; i++) {
       var firstLetter =names[i].charAt(0);
       if (firstLetter.toLocaleLowerCase() == "j") {
