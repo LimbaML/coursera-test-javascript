90fe25df229425221c3e8a0e9cc11ef005ef6bbc
LIMBA ML <127320850+LimbaML@users.noreply.github.com>
2023-05-30 20:34:41 +0530

    add file

---
+++ script1.js
@@ -0,0 +1,16 @@
+(
+  function(){
+
+    var names = ["yoo", "John", "Jack", "Jin", "Paul", "Rose", "Lassie", "Romeo", "Juliet", "Jacob"];
+    for ( var i = 0; i < names.length; i++) {
+      var firstLetter =names[i].charAt(0);
+      if (firstLetter.toLocaleLowerCase() == "j") {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+
+  }
+)
+();
